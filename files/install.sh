98ac7a3a1370443605cafb070396c9ce972de5c1
Seth Rutner <sprutner@gmail.com>
2017-02-13 21:39:20 -0800

    added docker compose script

--- files/install.sh
+++ files/install.sh
@@ -1,3 +1,5 @@
 sudo apt-get -y install docker.io
 sudo usermod -a -G docker ec2-user
 sudo service docker start
+sudo curl -L https://github.com/docker/compose/releases/download/2.11.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
