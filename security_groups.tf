b433280840da3f18c45af7dbadaca3fd516c0e88
Seth Rutner <sprutner@gmail.com>
2017-02-13 21:02:18 -0800

    fixed security groups to use aws_security_group_rule

--- security_groups.tf
+++ security_groups.tf
@@ -1,18 +1,22 @@
 resource "aws_security_group" "allow_me" {
   name = "allow_me"
   description = "Allow me to access"
+}
 
-  ingress {
+resource "aws_security_group_rule" "allow_ssh" {
+    type = "ingress"
     from_port = 22
     to_port = 22
     protocol = "tcp"
     cidr_blocks = ["67.169.94.5/32"]
+    security_group_id = "${aws_security_group.allow_me.id}"
   }
 
-  egress {
+resource "aws_security_group_rule" "allow_egress" {
+    type = "egress" 
     from_port = 0
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.allow_me.id}"
   }
-}
